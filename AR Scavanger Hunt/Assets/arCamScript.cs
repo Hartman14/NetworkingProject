9954efe738f70a73d9345edd62c8b8a99d15eeb8
Faith <milkgirl96@yahoo.com>
2018-04-20 00:44:40 -0500

    asdf

---
+++ AR Scavanger Hunt/Assets/arCamScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arCamScript : MonoBehaviour {
+
+    public GameObject webCameraPlane;
+    // Use this for initialization
+    void Start () {
+       
+
+
+        WebCamTexture webCameraTexture = new WebCamTexture();
+        webCameraPlane.GetComponent<MeshRenderer>().material.mainTexture = webCameraTexture;
+        webCameraTexture.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+
+    }
+}
