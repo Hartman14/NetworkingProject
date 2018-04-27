276993eae50bc60c8552349a1cab8962a9edd7de
Jordan <xoshadowhawkox@gmail.com>
2018-04-27 07:19:02 -0500

    GPS Addition & Inventory update
    -added inventory functionality and minor visualization change
    
    -added GPS location grabbing.

---
+++ AR Scavanger Hunt/Assets/Scripts/GPSFinder.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPSFinder : MonoBehaviour {
+
+    public static GPSFinder Instance { set; get; }
+
+    public float latitude;
+    public float longitude;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(this);
+        StartCoroutine(LocationServiceInitial());
+    }
+
+    private IEnumerator LocationServiceInitial()
+    {
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("User has not enabled GPS!");
+            yield break;
+        }
+
+        Input.location.Start();
+        int maxWait = 15;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if (maxWait <= 0)
+        {
+            Debug.Log("Timed Out");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Unable to determine device location");
+            yield break;
+        }
+
+        latitude = Input.location.lastData.latitude;
+        longitude = Input.location.lastData.longitude;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ AR Scavanger Hunt/Assets/Scripts/UpdateGPSText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateGPSText : MonoBehaviour{
+    public Text Coordinates;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Coordinates.text = "Lat: " + GPSFinder.Instance.latitude.ToString() + "\n Long: " + GPSFinder.Instance.longitude.ToString();
+    }
+}
