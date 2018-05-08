6b6bdcd76dc3932aa1a83b1e98f655604a860ddb
Jordan <xoshadowhawkox@gmail.com>
2018-05-08 14:52:30 -0500

    Quick Update
    Updated Vuforia DB, started adding checkpoints because I'm switching to my labtop from my desktop for the meeting

---
+++ AR Scavanger Hunt/Assets/CheckPointController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointController : MonoBehaviour {
+
+    public GameObject CheckPoint1;
+    public GameObject CheckPoint2;
+    public GameObject CheckPoint3;
+
+    // Use this for initialization
+    void Start () {
+        startHunt();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //resets the checkpoints for the scavanger hunt. turns off all but checkpoint 1
+    private void startHunt()
+    {
+        CheckPoint1.SetActive(true);
+        CheckPoint2.SetActive(false);
+        CheckPoint3.SetActive(false);
+    }
+
+
+}
---
+++ AR Scavanger Hunt/Assets/PlayerProgress.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProgress : MonoBehaviour {
+
+    public int currentCheckpoint;
+
+    // Use this for initialization
+    void Start () {
+        currentCheckpoint = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    //Sets the checkpoints to hidden,  initiates first checkpoint
+    private void setStart()
+    {
+
+    }
+
+}
